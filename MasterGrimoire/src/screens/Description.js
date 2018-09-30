36775670b511d0af6fadb649033b74c7ff8f2fa4
Josue Nascimento <josuetk63@gmail.com>
2018-09-30 11:38:33 -0300

    Add fake information

--- MasterGrimoire/src/screens/Description.js
+++ MasterGrimoire/src/screens/Description.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-import { TouchableOpacity, FlatList, Text, View } from "react-native";
+import { TouchableOpacity, FlatList, Text, View,StyleSheet,ScrollView } from "react-native";
 import {styles} from '../styles/PagStyles';
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000000"
-  },
-  content: {
-    marginLeft: 10,
-    fontSize: 20,
-    color :'white',
-  },
+
+const stylesDescription = StyleSheet.create({
+  title :{
+    fontSize:40,
+    fontWeight: 'bold'
+  }
 });
-*/
+
 class Description extends Component {
   state = {
 
@@ -37,10 +29,25 @@ class Description extends Component {
   };
   render() {
       return (
+    <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.content}>To aqui;</Text>
+        <Text style={styles.content}>
+          <Text style={stylesDescription.title}>Hit Points</Text>{'\n'}{'\n'}
+            <Text style={{fontSize:22,fontWeight:'bold'}}>Hit Dice:</Text> 1d12 per barbarian level.{'\n'}{'\n'}
+            <Text style={{fontSize:22,fontWeight:'bold'}}>Hit Points at 1st Level:</Text> 12 + your Constitution modifier.{'\n'}{'\n'}
+            <Text style={{fontSize:22,fontWeight:'bold'}}>Hit Points at Higher Levels:</Text> 1d12 (or 7) + your Constitution modifier per barbarian level after 1st.{'\n'}{'\n'}
+
+          <Text style={stylesDescription.title}>Proficiencies</Text>{'\n'}{'\n'}
+              <Text style={{fontSize:22,fontWeight:'bold'}}>Armor:</Text> Light armor, medium armor, shields.{'\n'}{'\n'}
+              <Text style={{fontSize:22,fontWeight:'bold'}}>Weapons:</Text> Simple weapons, martial weapons.{'\n'}{'\n'}
+              <Text style={{fontSize:22,fontWeight:'bold'}}>Tools:</Text> None.{'\n'}{'\n'}
+              <Text style={{fontSize:22,fontWeight:'bold'}}>Saving Throws:</Text> Strength, Constitution.{'\n'}{'\n'}
+              <Text style={{fontSize:22,fontWeight:'bold'}}>Skills:</Text> Choose two from Animal Handling, Athletics, Intimidation, Nature, Perception, and Survival.{'\n'}{'\n'}
+        </Text>
       </View>
-      );
+    </ScrollView>  
+
+    );
   }
 }
 export default Description;
