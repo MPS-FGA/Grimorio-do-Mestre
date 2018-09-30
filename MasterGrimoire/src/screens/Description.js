29ef55b78dd0d30bd4e4daac3f3060309d7b34ea
Josue Nascimento <josuetk63@gmail.com>
2018-09-30 09:25:49 -0300

    Create Description Screen

--- MasterGrimoire/src/screens/Description.js
+++ MasterGrimoire/src/screens/Description.js
@@ -2,5 +2,15 @@ import React, { Component } from "react";
 import { TouchableOpacity, FlatList, StyleSheet, Text, View } from "react-native";
 
 
+class Description extends Component {
+  state = {
 
+  };
+  render() {
+
+      return (
+      <Text >To aqui;</Text>
+    );
+  }
+}
 export default Description;
