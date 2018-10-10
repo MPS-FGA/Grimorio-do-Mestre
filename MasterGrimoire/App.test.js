51a9bba5d173f1b8cb7ed88b78eecfc76106ee00
FilipeKN4 <filipekn4@gmail.com>
2018-10-10 20:28:31 -0300

    Test if MainScreen renders

--- MasterGrimoire/App.test.js
+++ MasterGrimoire/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import App from './App';
+import MainScreen from './src/screens/MainScreen';
+import ContentList from './src/screens/ContentList';
+import MENU_ITENS from './src/constants/menuItens';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('renders MainScreen without crashing', () => {
+  const rendered = renderer.create(<MainScreen />).toJSON();
+  console.log(rendered);
   expect(rendered).toBeTruthy();
 });
