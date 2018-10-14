9d79af05578a452c3c07c76c7c260ec9bad69849
Hugo Carvalho <hugonvsc@gmail.com>
2018-10-14 20:16:45 -0300

    Update constants

---
+++ MasterGrimoire/src/constants/MenuItens.js
@@ -0,0 +1,18 @@
+const MENU_ITENS = [
+    {
+        "title": "References",
+        "screen":"Reference"
+    },
+    {
+        "title": "Create custom itens",
+        "screen":"Custom"
+    },
+    {
+        "title": "Manage campaigns",
+        "screen":"Campaign"
+    }
+
+
+]
+
+export default MENU_ITENS;
