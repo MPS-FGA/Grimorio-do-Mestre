ce9c7eecfbc4cf61a8633d13058cb8997dc22268
FilipeKN4 <filipekn4@gmail.com>
2018-10-14 21:16:28 -0300

    Add DetailPage snapshot test
    Co-authored-by: Breno Mariz <mrzbeo@gmail.com>

--- MasterGrimoire/App.test.js
+++ MasterGrimoire/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App';
 import MainScreen from './src/screens/MainScreen';
 import ContentList from './src/screens/ContentList';
+import DetailPage from './src/screens/DetailPage';
 
 import renderer from 'react-test-renderer';
 
@@ -17,17 +18,37 @@ describe('MainScreen test Suit', () => {
   });
 });
 
-describe('MainScreen test Suit', () => {
+describe('ContentList test Suit', () => {
   it('renders ContentList without crashing', () => {
     const navigation = {state: {
-      params: {
-        option: jest.fn()
-      }
-    },
-    setParams: jest.fn()
-  }
+        params: {
+          option: jest.fn()
+        }
+      },
+      setParams: jest.fn()
+    }
 
     const rendered = renderer.create(<ContentList navigation={navigation} />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+});
+
+describe('DetailPage test Suit', () => {
+  it('renders DetailPage without crashing', () => {
+    const navigation = {state: {
+        params : {
+          detailArg: {
+            pageInfo: jest.fn(),
+            item: {
+              name: jest.fn()
+            }
+          }
+        }
+      },
+      setParams: jest.fn()
+    }
+
+    const rendered = renderer.create(<DetailPage navigation={navigation} />).toJSON();
     expect(rendered).toMatchSnapshot();
   });
 });
